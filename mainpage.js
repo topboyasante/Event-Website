4caa12a4074833ec246624155ddf036d427d88ee
Nana K <topboyasante@gmail.com>
2022-07-03 09:19:20 +0000

    Initial commit

---
+++ mainpage.js
@@ -0,0 +1,8 @@
+let artisteText = document.getElementById('artistes');
+let djsText = document.getElementById('djs');
+function setOpacity(){
+        artisteText.classList.toggle("show");
+}
+function setOpacityDjs(){
+        djsText.classList.toggle("show");
+}
